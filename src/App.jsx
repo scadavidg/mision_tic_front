6af6b0922101e94d7ad6a467cac0e3a73c52e7a9
scadavidg <santiago@dondo.com>
2021-10-15 20:13:08 -0500

    añadiendo componentes para el control de acceso

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { NavBar } from './components/NavBar';
 import { ProductList } from './components/ProductList';
 import { CreateProduct } from './components/CreateProduct';
 import { EditProduct } from './components/EditProduct';
+import {Login} from "./components/Login";
+import {Signup} from "./components/Signup";
+import {Logout} from "./components/Logout";
 import { NotFound } from './components/NotFound';
 
 export function App() {
@@ -14,6 +17,9 @@ export function App() {
                 <Route exact path="/" component={ProductList} />
                 <Route exact path="/add" component={CreateProduct} />
                 <Route exact path="/edit/:id" component={EditProduct} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/registrarse" component={Signup} />
+                <Route exact path="/logout" component={Logout} />
                 <Route component={NotFound} />
             </Switch>
         </BrowserRouter>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Toolbar, makeStyles } from '@material-ui/core'
+import { AppBar, Toolbar, makeStyles, Button, Box } from '@material-ui/core'
 import { NavLink } from 'react-router-dom';
 
 const useStyle = makeStyles({
@@ -11,6 +11,14 @@ const useStyle = makeStyles({
         marginRight: 20,
         textDecoration: 'none',
         fontSize: 20
+    },
+    tab_end: {
+        color: '#FFFFFF',
+        marginRight: 20,
+        textDecoration: 'none',
+        fontSize: 20,
+        alignItems: 'end'
+        
     }
 })
 
@@ -19,11 +27,31 @@ export function NavBar() {
     const classes = useStyle();
 
     return (
-        <AppBar position="static" className={classes.header}>
-            <Toolbar>
-                <NavLink className={classes.tabs} to="/">Inicio</NavLink>
-                <NavLink className={classes.tabs} to="/add">Crear Producto</NavLink>
-            </Toolbar>
-        </AppBar>
+        <Box sx={{ display: 'flex', p: 1}}>
+            <AppBar position="static" className={classes.header}>
+                <Toolbar>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <NavLink className={classes.tabs} to="/">Inicio</NavLink>
+                        <NavLink className={classes.tabs} to="/add">Crear Producto</NavLink>
+                    </Box>
+                    <NavLink className={classes.tab_end} to="/registrarse">
+                        <Button color="secondary">
+                            registrarse
+                        </Button>
+                    </NavLink>
+                    <NavLink className={classes.tab_end} to="/login">
+                        <Button color="secondary">
+                            Login
+                        </Button>
+                    </NavLink>
+                    <NavLink className={classes.tab_end} to="/logout">
+                        <Button color="secondary">
+                            Logout
+                        </Button>
+                    </NavLink>
+                </Toolbar>
+            </AppBar>
+        </Box>
+       
     )
 }
