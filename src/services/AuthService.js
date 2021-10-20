be0a5f3d154e1ccb25b8ab6db7c93f056a30f83b
scadavidg <santiago@dondo.com>
2021-10-19 20:59:01 -0500

    update deploy

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 
 const authUrl = "https://peaceful-peak-03211.herokuapp.com/auth"; //prod
-//const authUrl = "http://localhost:3002/auth"; //dllo
+///const authUrl = "http://localhost:3002/auth"; //dllo
 
 export const loginAuth = async (credentials) => {
     return await axios.post(`${authUrl}/login`, credentials);
